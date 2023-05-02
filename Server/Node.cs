07f2faa30f24fe2cc7ac5d866ec6d0e158cf8073
nicolas <gruber.nicolas@student.htlwrn.ac.at>
2023-05-02 12:42:39 +0200

    Projektdateien hinzufügen.

---
+++ Server/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Node
+    {
+        private string desc;
+        private Dictionary<Node, int> neigh;
+        
+        public string Desc
+        {
+
+            get { return Desc; }
+            private set { Desc = value; } 
+        }
+
+        public Node(string desc) 
+        {
+            Desc= desc;
+        }
+
+        public bool AddNeigh(Node n, int weight) 
+        {
+            if(neigh.ContainsKey(n))
+            {
+                return false;
+            }
+            neigh[n] = weight;
+            return true;
+        }
+    }
+}
---
+++ Server/NodeManagment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class NodeManagment
+    {
+        private Dictionary<string, Node> nodes;
+        public NodeManagment() 
+        {
+            nodes = new Dictionary<string, Node>();
+        }
+
+        public bool AddNode(string desc) 
+        { 
+            if (nodes.ContainsKey(desc)) 
+            {
+                return false;
+            }
+            nodes[desc] = new Node(desc);
+            return true;
+        }
+        public bool AddConnection(string from, string to, int weight) 
+        {
+            return  nodes.ContainsKey(from) &&
+                    nodes.ContainsKey(to) &&
+                    nodes[from].AddNeigh(nodes[to], weight) && 
+                    nodes[to].AddNeigh(nodes[from], weight);
+        }
+    }
+}
